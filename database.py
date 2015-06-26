02e313fae429e864da4aff6138ca94073ad64ca9
cozziekuns <dingaling279@berkeley.edu>
2015-06-26 02:07:32 -0700

    fixed error when building database folder

--- database.py
+++ database.py
@@ -8,12 +8,17 @@ import os
 from renchon import db, Manga, Chapter, Page
 
 file_path = 'tmp/reader.db'
+
+# Create tmp folder if it doesn't exist
+if not os.path.exists('tmp'):
+    os.mkdir('tmp')
+
 # Clear old database if file exists
 if os.path.exists(file_path):
     os.remove(file_path)
-  
+
 # Make new database
 file = open(file_path, 'w+')
 file.close()
-  
+
 db.create_all()
