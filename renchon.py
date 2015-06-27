107a1078e5b28fbc8fe9e78b559540a0f3ba8da8
cozziekuns <dingaling279@berkeley.edu>
2015-06-26 17:59:19 -0700

    auto navigate to next chapter

--- renchon.py
+++ renchon.py
@@ -1,7 +1,7 @@
 #=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 # ** Renchon
 #------------------------------------------------------------------------------
-# Main module that creates the views, runs the database, and runs the app. Ok, 
+# Main module that creates the views, runs the database, and runs the app. Ok,
 # it basically does it all.
 #=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 
@@ -27,7 +27,7 @@ db = SQLAlchemy(app)
 #===============================================================================
 
 class Manga(db.Model):
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), index=True, unique=True)
     url = db.Column(db.String(100), unique=True)
@@ -37,7 +37,7 @@ class Manga(db.Model):
     cover = db.Column(db.String(150))
     description = db.Column(db.String(1000))
     last_updated = db.Column(db.DateTime)
-    
+
     def __init__(self, name, url, author, artist, status, cover, description):
         self.name = name
         self.url = url
@@ -47,71 +47,71 @@ class Manga(db.Model):
         self.cover = cover
         self.description = description
         self.last_updated = datetime.utcnow()
-        
+
     def __repr__(self):
         return "<User %r>" % (self.name)
-        
+
 #===============================================================================
 # ** Chapter
 #===============================================================================
-    
+
 class Chapter(db.Model):
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     num = db.Column(db.Float)
     date_added = db.Column(db.DateTime)
     manga_id = db.Column(db.Integer, db.ForeignKey("manga.id"))
-    manga = db.relationship("Manga", 
+    manga = db.relationship("Manga",
                             backref=db.backref("chapters", lazy="dynamic"))
-    
+
     def __init__(self, name, num, manga):
         self.name = name
         self.num = num
         self.manga = manga
         self.date_added = datetime.utcnow()
-        
+
     def __repr__(self):
         return "<Chapter %r>" % (self.name)
-    
+
 #===============================================================================
 # ** Page
 #===============================================================================
-    
+
 class Page(db.Model):
-    
+
     id = db.Column(db.Integer, primary_key=True)
     num = db.Column(db.Integer)
     image = db.Column(db.String(150))
     chapter_id = db.Column(db.Integer, db.ForeignKey("chapter.id"))
-    chapter = db.relationship("Chapter", 
+    chapter = db.relationship("Chapter",
                               backref=db.backref("pages", lazy="dynamic"))
-    
+
     def __init__(self, num, image, chapter):
         self.num = num
         self.image = image
         self.chapter = chapter
-        
+
     def __repr__(self):
         return "<Page %r>" % (self.num)
-        
+
 #===============================================================================
 # ** Helper Methods
 #===============================================================================
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1] in ALLOWED_EXTENSIONS
-    
+
 def rename(file, new_filename):
     return "".join([new_filename, ".", file.filename.split(".")[-1]])
-    
+
 def chapter_to_string(chapter_num):
     if int(chapter_num) == float(chapter_num):
         num_string = str(int(chapter_num))
     else:
         num_string = str(chapter_num)
     return num_string
-    
+
 def save_file(file, url, filename=None):
     if filename is None:
         filename = file.filename
@@ -124,16 +124,16 @@ def save_file(file, url, filename=None):
     # Then save the file to that directory
     file.save(complete_url)
     return complete_url
-  
+
 def delete_directory(url):
     shutil.rmtree(app.config['UPLOAD_FOLDER'] + url)
-  
+
 def recreate_chapter_list(chapter_list, chapter_before, latest_chapter):
     # If there is no chapter before, just insert an extra chapter at the top
     chapter_limit = 7
     if not chapter_before:
         chapter_limit += 1
-  
+
     # Recreate the list based on the position of the chapter we're currently
     # on
     if len(chapter_list) > chapter_limit:
@@ -144,24 +144,24 @@ def recreate_chapter_list(chapter_list, chapter_before, latest_chapter):
             if len(temp) == chapter_limit:
                 break
         chapter_list = temp
-        
+
     # Add the latest chapter
-    chapter_list.insert(0, [chapter_to_string(latest_chapter.num), 
+    chapter_list.insert(0, [chapter_to_string(latest_chapter.num),
         latest_chapter.name + " (Latest)"])
-        
+
     # Add the chapter before this chapter, unless this chapter is the earliest
     # chapter
     if chapter_before:
         info = [chapter_to_string(chapter_before.num), chapter_before.name]
         if not info in chapter_list:
             chapter_list.append(info)
-            
+
     return chapter_list
-        
+
 #===============================================================================
 # ** Views
 #===============================================================================
-        
+
 # Index
 @app.route("/reader")
 def index():
@@ -169,7 +169,7 @@ def index():
     manga_list = []
     cover_urls = {}
     chapters = {}
-    # Iterate through all the manga in the database (eventually this will be 
+    # Iterate through all the manga in the database (eventually this will be
     # shortened to the first 50 chapters or so...)
     for manga in Manga.query.all():
         # Filler
@@ -180,7 +180,7 @@ def index():
             chapters[manga.name] = newest_chapter.name
         else:
             chapters[manga.name] = ""
-    return render_template("index.html", manga_list=manga_list, 
+    return render_template("index.html", manga_list=manga_list,
                             cover_urls=cover_urls, chapters=chapters)
 
 # Admin
@@ -188,71 +188,71 @@ def index():
 def admin():
     manga = Manga.query.all()
     manga_list = list(map(lambda x: x.name, manga))
-    chapter_list = list(map(lambda x: list(map(lambda y: [y.name, y.num], 
+    chapter_list = list(map(lambda x: list(map(lambda y: [y.name, y.num],
         x.chapters.all())), manga))
-    return render_template("admin.html", manga=manga_list, 
+    return render_template("admin.html", manga=manga_list,
         chapter_list=chapter_list)
-                                    
+
 # Add Manga
 @app.route("/reader/add_manga", methods=["POST"])
 def add_manga():
     name = request.form["manga_name"]
     url = request.form["manga_url"]
     file = request.files["manga_cover"]
-    
+
     # Handle the case where there are conflicting manga names
     if Manga.query.filter_by(name=name).first():
         error_text = "Manga title already exists."
         return render_template('admin.html', error=error_text)
-    
+
     # Handle the case where there are conflicting manga urls
     if os.path.exists(app.config['UPLOAD_FOLDER'] + url):
         return render_template('admin.html', error="Manga URL already exists.")
-    
+
     # Handle the case where the file is not sent
     if not (file and allowed_file(file.filename)):
         return render_template('admin.html', error="Invalid Cover File.")
-    
+
     # Save the file to the server
     cover_filename = rename(file, "cover")
     cover_url = save_file(file, url, cover_filename)
-    
+
     # Then, just dump the contents into the database
     author = request.form["manga_author"]
     artist = request.form["manga_artist"]
     status = request.form["manga_status"]
     description = request.form["manga_description"]
-    new_manga = Manga(name, url, author, artist, status, cover_url, 
+    new_manga = Manga(name, url, author, artist, status, cover_url,
         description)
-    
+
     # Add and commit the new manga into the database
     db.session.add(new_manga)
     db.session.commit()
 
     return redirect(url_for("admin"))
-    
+
 # Add Chapter
 @app.route("/reader/add_chapter", methods=["POST"])
 def add_chapter():
     name = request.form["chapter_name"]
     num = 0
-    
-    # Convert the chapter number to a float (we need a float for stuff like 
+
+    # Convert the chapter number to a float (we need a float for stuff like
     # omakes)
     try:
         num = float(request.form["chapter_num"])
     except ValueError:
         return render_template('admin.html', error="Invalid Chapter Number.")
-      
+
     manga = Manga.query.filter_by(name=request.form["chapter_manga"]).first()
     new_chapter = Chapter(name, float(num), manga)
-    
+
     # Rename and add all the chapter pages
     curr_page = 1
-    
+
     # Representation is different if its a number
     num_string = chapter_to_string(num)
-        
+
     # Make a new folder for the chapter
     url = new_chapter.manga.url + "/Chapter_" + num_string
     pages = request.files.getlist("chapter_pages")
@@ -265,12 +265,12 @@ def add_chapter():
         db.session.add(new_page)
         # Remember to increment curr_page
         curr_page += 1
-    
+
     db.session.add(new_chapter)
     db.session.commit()
-    
+
     return redirect(url_for("admin"))
-    
+
 # Delete Chapter
 @app.route("/reader/delete_chapter", methods=["POST"])
 def delete_chapter():
@@ -278,72 +278,79 @@ def delete_chapter():
         name=request.form["chapter_delete_manga"]).first()
     chapter = manga.chapters.filter_by(
       num=request.form["chapter_delete_chapter"]).first()
-      
+
     num_string = chapter_to_string(chapter.num)
-    url = chapter.manga.url + "/Chapter_" + num_string 
+    url = chapter.manga.url + "/Chapter_" + num_string
     delete_directory(url)
-    
+
     db.session.delete(chapter)
     db.session.commit()
-    
+
     return redirect(url_for("admin"))
-    
+
 # Manga Summary Page
 @app.route("/reader/<manga>")
 def view_manga(manga=None):
     # Redirect to the index
     if not manga:
         redirect(url_for("index"))
-        
+
     # Make sure that a manga with that url exists
     manga = Manga.query.filter_by(url=manga).first()
     if manga is None:
         return render_template("404.html"), 404
-        
+
     return render_template("manga.html", manga=manga)
-    
+
 # Reader
 @app.route("/reader/<manga>/<chapter>")
 def view_page(manga=None, chapter=None):
     # Redirect to the summary page
     if not chapter:
         redirect(url_for("view_manga", manga))
-        
+
     # Make sure that a manga with that url exists
     manga = Manga.query.filter_by(url=manga).first()
     if manga is None:
         return render_template("404.html"), 404
-        
+
     # Make sure that the manga has a chapter with that number
     try:
         chapter_num = float(chapter)
     except ValueError:
         return render_template("404.html"), 404
-        
+
     results = manga.chapters.filter_by(num=chapter_num)
     chapter = results.first()
     if chapter is None:
         return render_template("404.html"), 404
-        
+
     last_page = chapter.pages.order_by("-num").first()
-    
+
     # Not going to do this in JS
     num_string = chapter_to_string(chapter_num)
     urls = list(map(lambda x: "/" + x.image, chapter.pages.all()))
-    
+
     chapters = manga.chapters.order_by("-num").all()
     chapter_list = []
     latest_chapter = chapters[0]
-    
-    # Find the chapter before this one, unless this is the last chapter
+
+    # Find the chapter before this one, unless this is the first chapter
+    # Remember that chapters are ordered descendingly
     if chapters.index(chapter) == len(chapters) - 1:
         chapter_before = None
     else:
         chapter_before = chapters[chapters.index(chapter) + 1]
-        
+
+    # Find the chapter after this one, unless this is the last chapter
+    if chapters.index(chapter) == 0:
+        chapter_after = None
+    else:
+        chapter_after = chapters[chapters.index(chapter) - 1]
+
     # List automatically contains the most updated chapter
     contains_chapter = (chapter == latest_chapter)
-    
+
     for item in chapters[1:]:
         chapter_list.append([chapter_to_string(item.num), item.name])
         # Set the flag to true if the list contains the chapter
@@ -351,19 +358,26 @@ def view_page(manga=None, chapter=None):
             contains_chapter = True
         if (len(chapter_list) >= 7 and contains_chapter):
             break
-            
-    chapter_list = recreate_chapter_list(chapter_list, chapter_before, 
+
+    chapter_list = recreate_chapter_list(chapter_list, chapter_before,
         latest_chapter)
-    
-    return render_template("reader.html", manga=manga.name, 
-                          chapter_name=chapter.name, chapter_num=num_string, 
-                          urls=urls, last_page=last_page.num, 
-                          chapters=chapter_list)
+
+    # Get the next chapter number for the reader
+    if chapter_after:
+        next_chapter = chapter_to_string(chapter_after.num)
+    else:
+        next_chapter = -1
+
+    return render_template("reader.html", manga=manga.name,
+                          chapter_name=chapter.name, chapter_num=num_string,
+                          urls=urls, last_page=last_page.num,
+                          chapters=chapter_list,
+                          next_chapter=next_chapter)
 
 # Page Not Found
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
--- static/renchon.js
+++ static/renchon.js
@@ -9,6 +9,8 @@ var fullscreen_enabled = false;
 
 var key_triggered = 0;
 var page_urls = [];
+var previous_chapter = -1;
+var next_chapter = -1;
 
 var pan_start_x = -1;
 var pan_start_y = -1;
@@ -19,25 +21,25 @@ var manga_image = document.getElementById("manga");
 /* Preload Images */
 
 function preload() {
-  
+
   if (curr_page + 3 > last_page) {
     limit = last_page;
   } else {
     limit = curr_page + 3;
   }
-  
+
   for (i = curr_page - 1; i < limit; i++) {
-    
+
     // Only preload images that haven't been cached
     if (cache[i]) {
       continue;
     }
-    
+
     img = new Image();
     img.src = page_urls[i];
     cache[i] = img;
   }
-  
+
 }
 
 /* Input Handling */
@@ -46,9 +48,9 @@ document.onkeydown = check_key_down;
 document.onkeyup = check_key_up;
 
 function check_key_down(e) {
-  
+
   e = e || window.event;
-  
+
   // Down
   if (e.keyCode == '40' && fullscreen_enabled) {
     scroll_down();
@@ -68,9 +70,9 @@ function check_key_down(e) {
   } else if (e.keyCode == '38' && fullscreen_enabled) {
     scroll_up();
   }
-  
+
   key_triggered = e.keyCode;
-  
+
 }
 
 function check_key_up(e) {
@@ -86,28 +88,28 @@ function check_key_up(e) {
 
 /* Fullscreen Handling */
 
-$(document).on("webkitfullscreenchange mozfullscreenchange fullscreenchange", 
+$(document).on("webkitfullscreenchange mozfullscreenchange fullscreenchange",
 
   function() {
-    
-    fullscreen_enabled = document.fullscreenElement || 
-                        document.mozFullScreenElement || 
+
+    fullscreen_enabled = document.fullscreenElement ||
+                        document.mozFullScreenElement ||
                         document.webkitFullscreenElement;
-    
+
     if (fullscreen_enabled) {
       center_image();
     } else {
       revert_image();
     }
-    
+
   }
-  
+
 );
 
 function launch_fullscreen() {
 
   element = document.getElementById("fullscreen");
-  
+
   if (element.requestFullscreen) {
     element.requestFullscreen();
   } else if (element.mozRequestFullScreen) {
@@ -117,47 +119,47 @@ function launch_fullscreen() {
   } else if (element.msRequestFullscreen) {
     element.msRequestFullscreen();
   }
-  
+
 }
 
 function center_image() {
-  
+
   document.getElementById("fullscreen").style.padding = "0px";
-  
+
   manga_image.onclick = next_page;
   manga_image.onmousedown = start_pan;
   manga_image.onmousemove = process_pan;
   manga_image.onmouseup = end_pan;
   manga_image.onmouseout = end_pan;
   manga_image.style.maxWidth = "initial";
-  
+
   resize_image();
-  
+
   center_width();
   center_height();
-   
+
 }
 
 function revert_image() {
-  
+
   document.getElementById("fullscreen").style.padding = "32px";
-  
+
   manga_image.onclick = launch_fullscreen;
   manga_image.onmousedown = null;
   manga_image.onmousemove = null;
   manga_image.onmouseup = null;
   manga_image.onmouseout = null;
-  
+
   manga_image.style.marginLeft = "auto";
   manga_image.style.marginTop = "auto";
   manga_image.style.height = "initial";
   manga_image.style.width = "initial";
   manga_image.style.maxWidth = "80%";
-  
+
 }
 
 function resize_image() {
-  
+
   if (cache[curr_page - 1].width > screen.width) {
     // Landscape (usually spreads)
     if (cache[curr_page - 1].width > cache[curr_page - 1].height) {
@@ -176,7 +178,7 @@ function resize_image() {
         manga_image.style.height = cache[curr_page - 1].height.toString() + "px";
       }
     } else {
-      // Portrait 
+      // Portrait
       width_ratio = screen.width / cache[curr_page].width;
       height_num = cache[curr_page - 1].height * width_ratio;
       manga_image.style.width = screen.width.toString() + "px";
@@ -186,57 +188,57 @@ function resize_image() {
     manga_image.style.width = cache[curr_page - 1].width.toString() + "px";
     manga_image.style.height = cache[curr_page - 1].height.toString() + "px";
   }
-  
+
 }
 
 function center_width() {
-  
+
   manga_image.style.marginLeft = "0px";
-  
+
   if (parseInt(manga_image.style.width) < screen.width) {
     x_pos = (screen.width - parseInt(manga_image.style.width)) / 2;
     manga_image.style.marginLeft = x_pos.toString() + "px";
   }
-  
+
 }
 
 function center_height() {
-  
+
   manga_image.style.marginTop = "0px";
-  
+
   if (parseInt(manga_image.style.height) < screen.height) {
     y_pos = (screen.height - parseInt(manga_image.style.height)) / 2;
     manga_image.style.marginTop = y_pos.toString() + "px";
   }
-  
+
 }
 
 /* Fullscreen Controls */
 
 function scroll_down() {
-  
+
   manga_image = document.getElementById("manga");
-  
+
   // Only scroll if the page is greater than the height of the screen
   if (parseInt(manga_image.style.height) > screen.height) {
     max_margin = screen.height - parseInt(manga_image.style.height);
-    new_margin = Math.max(max_margin, 
+    new_margin = Math.max(max_margin,
         parseInt(manga_image.style.marginTop) - 32)
     manga_image.style.marginTop = new_margin.toString() + "px";
   }
-  
+
 }
 
 function scroll_up() {
-  
+
   manga_image = document.getElementById("manga");
-  
+
   // Only scroll if the page is greater than the height of the screen
   if (parseInt(manga_image.style.height) > screen.height) {
     new_margin = Math.min(0, parseInt(manga_image.style.marginTop) + 32)
     manga_image.style.marginTop = new_margin.toString() + "px";
   }
-  
+
 }
 
 /* Mouse Scroll */
@@ -252,122 +254,131 @@ function scroll_mouse(event) {
   } else if (delta == 1) {
     scroll_up();
   }
-  
+
 }
 
 /* Mouse Panning */
 
 function start_pan(event) {
-  
+
   pan_start_x = event.clientX;
   pan_start_y = event.clientY;
-  
+
 }
 
 function process_pan(event) {
-  
+
   if (pan_start_x >= 0 && pan_start_y >= 0) {
     x_diff = event.clientX - pan_start_x;
     y_diff = event.clientY - pan_start_y;
-    
+
     dest_x = parseInt(manga_image.style.marginLeft) + x_diff
     dest_y = parseInt(manga_image.style.marginTop) + y_diff
-    
-    // Only pan horizontally if the width of the page is greater 
+
+    // Only pan horizontally if the width of the page is greater
     // than the width of the screen
     if (parseInt(manga_image.style.width) > screen.width) {
       max_margin = screen.width - parseInt(manga_image.style.width);
       new_margin = Math.max(Math.min(0, dest_x), max_margin);
       manga_image.style.marginLeft = new_margin.toString() + "px";
     }
-    
-    // Only scroll if the height of the page is greater than the 
+
+    // Only scroll if the height of the page is greater than the
     // height of the screen
     if (parseInt(manga_image.style.height) > screen.height) {
       max_margin = screen.height - parseInt(manga_image.style.height);
       new_margin = Math.max(Math.min(0, dest_y), max_margin);
       manga_image.style.marginTop = new_margin.toString() + "px";
     }
-    
+
     pan_start_x = event.clientX;
     pan_start_y = event.clientY;
   }
-  
+
 }
 
 function end_pan() {
-  
+
   pan_start_x = -1;
   pan_start_y = -1;
-  
+
+}
+
+/* Chapter Handling */
+function goto_chapter(chapter) {
+  current_url = window.location.href;
+  window.location.href = current_url.replace(/\d+$/, chapter);
 }
 
 /* Page Handling */
 
 function goto_page(page) {
 
-  preload();
   curr_page = page;
   manga_image.src = page_urls[curr_page - 1];
-  
+
   if (fullscreen_enabled) {
     center_image();
   } else {
     window.scrollTo(0, 0);
   }
-  
-  
+
+  preload();
   update_navbar_page();
 
 }
 
 function next_page() {
-  
+
   if (curr_page < last_page) {
     goto_page(curr_page + 1);
+  } else {
+    if (parseInt(next_chapter) >= 0) {
+      goto_chapter(next_chapter);
+    }
   }
-  
-} 
+
+}
 
 function previous_page() {
-  
+
   if (curr_page > 1) {
     goto_page(curr_page - 1);
   }
-  
+
 }
 
 function update_navbar_page() {
-  
+
   dropdown = document.getElementById("dropdown_page")
   document.getElementById("page_num").innerHTML = curr_page.toString();
-  
+
   start_page = Math.max(curr_page - 3, 2)
-  
+
   if (start_page == 2) {
     end_page = Math.min(8, last_page - 1)
   } else {
     end_page = Math.min(curr_page + 3, last_page - 1)
   }
-  
+
   if (end_page == last_page - 1) {
     start_page = Math.max(last_page - 1 - 6, 2);
   }
-  
+
   dropdown.innerHTML = "";
-  
+
   // First Page
-  dropdown.innerHTML += "<a href='#' onclick='goto_page(1)'>" + 
-      "<li>Page 1 (First)</li></a>"; 
-  
+  dropdown.innerHTML += "<a href='#' onclick='goto_page(1)'>" +
+      "<li>Page 1 (First)</li></a>";
+
   for (i = start_page; i <= end_page; i++) {
-    dropdown.innerHTML += "<a href='#' onclick='goto_page(" + i.toString() + 
-        ")'>" + "<li>Page " + i.toString() + "</li></a>"; 
+    dropdown.innerHTML += "<a href='#' onclick='goto_page(" + i.toString() +
+        ")'>" + "<li>Page " + i.toString() + "</li></a>";
   }
-  
+
   // And Last Page
-  dropdown.innerHTML += 
-      "<a href='#' onclick='goto_page(" + last_page.toString() + 
+  dropdown.innerHTML +=
+      "<a href='#' onclick='goto_page(" + last_page.toString() +
       ")'>" + "<li>Page " + last_page.toString() + " (Last)</li></a>";
-  
+
 }
