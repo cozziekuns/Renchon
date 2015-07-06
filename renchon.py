7a7aa9ed1dc9bf334fc8b6c7b6639a521f2b74a3
cozziekuns <dingaling279@berkeley.edu>
2015-07-06 16:48:02 -0700

    database no longer clears on new depoi

--- renchon.py
+++ renchon.py
@@ -479,13 +479,12 @@ def setup_database(*args, **kwargs):
     # Create tmp folder if it doesn't exist
     if not os.path.exists('tmp'):
         os.mkdir('tmp')
-    # Clear old database if file exists
-    if os.path.exists(file_path):
-        os.remove(file_path)
-    # Make new database
-    file = open(file_path, 'w+')
-    file.close()
-    db.create_all()
+    # If an old database does not exist
+    if not os.path.exists(file_path):
+        # Make new database
+        file = open(file_path, 'w+')
+        file.close()
+        db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
