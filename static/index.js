a26cca0f66885ce62b8706313a8a9f785d81deb0
cozziekuns <dingaling279@berkeley.edu>
2015-07-08 19:48:43 -0700

    style fixes

--- static/index.js
+++ static/index.js
@@ -1,16 +1,21 @@
 window.onresize = move_tiles;
 
 function move_tiles() {
+
   wrapper_width = $("#main_wrapper").width();
   tiles = document.getElementsByClassName("manga_tile")
+
   columns = 1
   while ((columns + 1) * (tiles[0].offsetWidth + 64) - 64 < wrapper_width) {
     columns += 1
   }
   columns = Math.min(columns, tiles.length);
+  
   total_margin = columns * (tiles[0].offsetWidth + 64) - 64;
   new_margin = (wrapper_width - total_margin) / 2 - 1;
+
   for (i = 0; i < tiles.length; i++) {
+
     if (i % columns == 0) {
       tiles[i].style.marginLeft = new_margin.toString() + "px"
       tiles[i].style.marginRight = "32px";
@@ -21,8 +26,9 @@ function move_tiles() {
       tiles[i].style.marginLeft = "32px";
       tiles[i].style.marginRight = "32px";
     }
-  }
-}
 
+  }
+
+}
 
 move_tiles();
